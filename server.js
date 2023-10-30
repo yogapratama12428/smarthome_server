c2d6901ff11f256000cb31acc32bfdb2e594bb47
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2023-10-30 15:40:48 +0700

    initial project

---
+++ server.js
@@ -0,0 +1,239 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config()
+import mongoose from 'mongoose';
+
+const app = express()
+
+const { MONGO_URL, PORT } = process.env
+
+    mongoose
+    .connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB is  connected successfully"))
+    .catch((err) => console.error(err));
+
+    const DeviceSchema = new mongoose.Schema({
+        name: {
+          type: String,
+          required: [true, "Your name is required"],
+          unique: true,
+        },
+        value: {
+          type: Boolean,
+          required: [true, "Your state is required"],
+        },
+        createdAt: {
+          type: Date,
+          default: new Date(),
+        },
+        updatedAt: {
+        type: Date,
+        default: new Date(),
+        }
+    });
+    
+    const Device = mongoose.model("Device", DeviceSchema)
+
+    const SensorSchema = new mongoose.Schema({
+        name: {
+          type: String,
+          required: [true, "Your name is required"],
+          unique: true,
+        },
+        value: {
+          type: Number,
+          required: [true, "Your state is required"],
+        },
+        createdAt: {
+          type: Date,
+          default: new Date(),
+          default: new Date(),
+        },
+        updatedAt: {
+        type: Date,
+        }
+    });
+    
+    const Sensor = mongoose.model("Sensor", SensorSchema)
+
+
+app.use(cors({
+    origin: 'http://localhost:5000'
+}))
+
+app.use(express.json())
+
+app.get('/', async (res) => {
+    res.statusCode(200).json({
+        'msg': 'hello world'
+    })
+})
+
+app.get('/device', async (req, res) => {
+    try {
+        const response = await Device.find({})
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+
+app.get('/device/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const response = await Device.findOne({_id : id})
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+
+app.post('/device', async (req, res) => {
+
+    const {  name, value } = req.body
+    try {
+        const response = await Device.create({
+            name: name,
+            value: value
+        })
+
+        res.status(201).json(response)
+        
+    } catch (error) {
+        res.status(401).json(error)
+    }
+})
+
+app.put('/device/:id', async (req, res) => {
+
+    const {  name, value } = req.body
+
+    const { id } = req.params
+    try {
+
+        const user = await Device.findOne({ _id: id })
+
+        if (!user) {
+            return res.status(404).json({ msg: 'Not Found' });
+        }
+
+        const response = await Device.findOneAndUpdate(
+            {
+              _id: id 
+            },
+            {
+              $set: { name, value }
+            },
+            {
+                new: true
+            }
+        )
+
+        res.status(201).json({'message': 'updated successfully'})
+        
+    } catch (error) {
+        res.status(401).json(error)
+    }
+})
+
+app.delete('/device/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+
+        const user = await Device.findOne({ _id: id })
+
+        if (!user) {
+            return res.status(404).json({ msg: 'Not Found' });
+        }
+        
+        await Device.findOneAndDelete({ _id: id })
+
+        res.status(201).json({ 'msg' : 'Device deleted successfully'})
+
+    } catch (error) {
+        res.status(401).json(error)
+    }
+    
+})
+
+app.get('/sensor', async (req, res) => {
+    try {
+        const response = await Sensor.find({})
+        res.status(200).json(response)
+    } catch (err) {
+        res.status(404).json(err)
+    }
+})
+
+app.get('/sensor/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const response = await Sensor.findOne({ _id: id})
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json(err)
+    }
+})
+
+app.post('/sensor', async (req, res) => {
+
+    const { name, value } = req.body
+
+    try {
+        const response = await Sensor.create({
+            name, 
+            value
+        })
+        res.status(201).json(response)
+    } catch (err) {
+        res.status(401).json(err)
+    }
+})
+
+app.put('/sensor/:id', async (req, res) => {
+    const { id } = req.params
+    const { name, value } = req.body
+
+    try {
+        const user = await Sensor.findOne({ _id: id })
+
+        if(!user) return res.status(404).json({ 'message': 'sensor not found' })
+
+        await Sensor.findOneAndUpdate(
+            { _id : id },
+            { $set : { name, value}  },
+            { new: true}
+        )
+
+        res.status(201).json({'message': 'updated successfully'})
+    } catch (error) {
+        res.status(401).json({'message': error.message})
+    }
+})
+
+
+app.delete('/sensor/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const user = await Sensor.findOne({ _id: id })
+
+        if(!user) return res.status(404).json({'message': ' sensor not found'})
+
+        await Sensor.findOneAndDelete({ _id: id})
+
+        res.status(201).json({ 'msg' : 'Device deleted successfully'})
+    } catch (error) {
+        res.status(404).json({'message': error.message})
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log('listening on port ' + PORT)
+    
+})
